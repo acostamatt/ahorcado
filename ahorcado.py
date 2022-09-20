79484236a74719d2f6549ba56576b81ee8eb1768
valentinlarrabeitia <valentin.larrabeitia@alumnos.ispi4038.edu.ar>
2022-09-20 13:31:48 -0300

    Agregado de colores

--- ahorcado.py
+++ ahorcado.py
@@ -4,18 +4,21 @@ from time import sleep
 from colorama import Fore, init
 from funciones_ahorcado import saltear_lineas_hasta_L7, select_top5_ptajes, separador, ahorcar, mostrar_estado, faltantes
 os.system('clear')
-print('\nBIENVENIDOS AL AHORCADO')
+init()
+print(f'\n{Fore.MAGENTA}BIENVENIDOS AL AHORCADO{Fore.WHITE}')
 archivo_usuarios = open('usuarios.txt', 'a')
 archivo_usuarios.close
 
 archivo_usuarios = open('usuarios.txt', 'r')
 while True:
-    usuario = input('\nIngrese su nombre de usuario:').strip()
+    usuario = input('\nIngrese su nombre de usuario: ').strip()
     if usuario.isdigit() is True:
+        os.system('clear')
         print('\nSu nombre de usuario debe constar de letras y números.')
         continue    
     elif usuario + '\n' in archivo_usuarios.readlines():
         archivo_usuarios.seek(0)
+        os.system('clear')
         print('\nYa se encuentra registrado ese usuario. Intente con otro nombre.')
         continue
     else:
@@ -27,15 +30,12 @@ archivo_usuarios = open('usuarios.txt', 'a')
 archivo_usuarios.write(usuario + '\n')
 palabras = open('spanish.lst', 'r')
 
-#Inicializo la función init de colorama, para añadir color a los textos referidos a la dificultad.
-init()
-
-print('Usuario registrado correctamente.')
+print(f'{Fore.MAGENTA}Usuario registrado correctamente.{Fore.WHITE}')
 sleep(3)
 os.system('clear')
 
 while True:
-    dificultad = input('\nSeleccione la dificultad deseada:\n'+Fore.GREEN+'Facil: F\n'+Fore.BLUE+'Normal: N\n'+Fore.RED+'Dificil: D\n\n'+Fore.WHITE+'Escriba aqui la letra que corresponda con su dificultad:  ').strip()
+    dificultad = input('\nSeleccione la dificultad deseada:\n'+Fore.GREEN+'Facil: F\n'+Fore.YELLOW+'Normal: N\n'+Fore.RED+'Dificil: D\n\n'+Fore.WHITE+'Escriba aqui la letra que corresponda con su dificultad:  ').strip()
     palabras_para_adivinar = []    
     if dificultad.upper() == 'F':
         print('Ha seleccionado el modo Facil.')
@@ -94,16 +94,16 @@ while True:
                     print('\nLa letra es incorrecta.')
                     break
     
-        ahorcado = ahorcar(vidas)            
+        ahorcado = f'{Fore.RED}{ahorcar(vidas)}{Fore.WHITE}'            
         if vidas == 0:
             perdiste += 1
             print(ahorcado)
             sleep(3)
             os.system('clear')
-            print(f'    GAME OVER\nLa palabra era: {palabra_a_adivinar}')
+            print(f'       {Fore.RED}GAME OVER{Fore.WHITE} \nLa palabra era: {palabra_a_adivinar}')
             break
-        
-        estatus_actual = mostrar_estado(palabra_a_adivinar, letras_adivinadas)  
+ 
+        estatus_actual = f'{Fore.GREEN}{mostrar_estado(palabra_a_adivinar, letras_adivinadas)}{Fore.WHITE}'
         letras_faltantes = faltantes(palabra_a_adivinar, letras_adivinadas)
         
         print(f'Le quedan {vidas} vidas.\n{ahorcado}')
@@ -111,14 +111,14 @@ while True:
         
         if letras_faltantes == 0:
             os.system('clear')
-            print('\nFelicidades haz ganado')
+            print(f'\n{Fore.GREEN}Felicidades haz ganado')
             print('La palabra es: ' + palabra_a_adivinar)
             puntaje_total += pts_por_esta_palabra
-            print(f'Obtuvo: {pts_por_esta_palabra} pts. | Puntos acumulados: {puntaje_total} \n\n')        
+            print(f'Obtuvo: {pts_por_esta_palabra} pts. | Puntos acumulados: {puntaje_total}{Fore.RESET}')        
             break
 
     if perdiste == 1:
-            print(f'Su puntaje fue: {puntaje_total}')
+            print(f'Su puntaje fue: {Fore.CYAN}{puntaje_total}')
             break
 
 archivo_pts = open('puntajes.txt', 'a')
@@ -127,11 +127,11 @@ archivo_pts.close
 
 archivo_usuarios = open('usuarios.txt', 'r')
 archivo_pts = open('puntajes.txt', 'r')
-print('\n' + '-'*30)
+print(Fore.LIGHTMAGENTA_EX + '\n' + '-'*30)
 print('Top 5 mejores puntajes')
 print('-'*30)
 for nombre_usuario, puntaje in select_top5_ptajes(archivo_usuarios, archivo_pts).items():
-        print(str(nombre_usuario).rstrip('\n') + ' '*(20-len(nombre_usuario)) + ': ' + str(puntaje).rstrip('\n'))
+        print(str(nombre_usuario).rstrip('\n') + ' '*(20-len(nombre_usuario)) + ': ' + Fore.CYAN + str(puntaje).rstrip('\n') + Fore.LIGHTMAGENTA_EX)
         print('-'*30)
 archivo_pts.close
 archivo_usuarios.close
